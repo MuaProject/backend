48b39e3168345767e08b004f20ab13e85c160bf2
jae-2024 <dbswotkd2019@naver.com>
2025-11-20 13:44:04 +0900

    Initial commit

---
+++ src/main/java/Mua/Mua_backend/MuaBackendApplication.java
@@ -0,0 +1,13 @@
+package Mua.Mua_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Mua/Mua_backend/MuaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Mua.Mua_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
