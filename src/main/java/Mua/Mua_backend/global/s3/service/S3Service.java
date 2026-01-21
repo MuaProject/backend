19ec1b4f93bd9c00ca033fb186fed26888adefba
jae-2024 <dbswotkd2019@naver.com>
2026-01-21 17:32:28 +0900

    [#1] Setting: S3 설정 수정

--- src/main/java/Mua/Mua_backend/global/s3/service/S3Service.java
+++ src/main/java/Mua/Mua_backend/global/s3/service/S3Service.java
@@ -3,8 +3,11 @@ package Mua.Mua_backend.global.s3.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
 import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
 
 import java.net.URL;
@@ -15,6 +18,7 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class S3Service {
 
+    private final S3Client s3Client;
     private final S3Presigner presigner;
 
     @Value("${cloud.aws.s3.bucket}")
@@ -44,5 +48,32 @@ public class S3Service {
                 );
 
         return presignedRequest.url();
+    }
+
+    public URL generatePreSignedGetUrl(String key) {
+        GetObjectPresignRequest request =
+                GetObjectPresignRequest.builder()
+                        .signatureDuration(Duration.ofMinutes(5))
+                        .getObjectRequest(r -> r
+                                .bucket(bucket)
+                                .key(key)
+                        )
+                        .build();
+
+        return presigner.presignGetObject(request).url();
+    }
+
+    public void delete(String key) {
+        if (key == null || key.isBlank()) {
+            return;
+        }
+
+        DeleteObjectRequest deleteRequest =
+                DeleteObjectRequest.builder()
+                        .bucket(bucket)
+                        .key(key)
+                        .build();
+
+        s3Client.deleteObject(deleteRequest);
     }
 }
