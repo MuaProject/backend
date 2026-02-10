ed62ac5bd78715a76e79abe63a8d71b36be51c4f
jae-2024 <dbswotkd2019@naver.com>
2026-02-10 21:29:25 +0900

    [#34] Feat: notifications 엔티티 & enum

---
+++ src/main/java/Mua/Mua_backend/domain/notification/entity/NotificationType.java
@@ -0,0 +1,8 @@
+package Mua.Mua_backend.domain.notification.entity;
+
+public enum NotificationType {
+    PARTICIPATION_APPROVED,
+    PARTICIPATION_REJECTED,
+    GAME_STARTED,
+    GAME_FINISHED
+}
