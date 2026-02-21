f5863530d38d10c968269040831cf25c657af96e
jae-2024 <dbswotkd2019@naver.com>
2026-02-21 22:51:58 +0900

    [#52] Refactor: redirect 제거 및 JWT 응답 구조 개선

--- src/main/java/Mua/Mua_backend/global/security/oauth/OAuth2LoginSuccessHandler.java
+++ src/main/java/Mua/Mua_backend/global/security/oauth/OAuth2LoginSuccessHandler.java
@@ -61,8 +61,17 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
                 .maxAge(60 * 60 * 24 * 14)
                 .build();
 
-        response.setHeader("Set-Cookie", refreshCookie.toString());
+        response.addHeader("Set-Cookie", refreshCookie.toString());
 
-        response.sendRedirect(redirectUri + "?token=" + accessToken);
+        // AccessToken을 JSON으로 반환
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        String jsonResponse = String.format(
+                "{\"accessToken\":\"%s\"}", accessToken
+        );
+
+        response.getWriter().write(jsonResponse);
+        response.setStatus(HttpServletResponse.SC_OK);
     }
 }
